6a01a8a585e65800e969bbe1d9afa4ec78446eb6
xpowder <kadmarmouad8@gmail.com>
2025-09-29 15:57:19 +0100

    fix

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,31 +1,24 @@
-from rest_framework import viewsets, permissions, filters
+from rest_framework import viewsets, permissions, filters, status
 from rest_framework.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
-from django_filters.rest_framework import DjangoFilterBackend
-
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 from .permissions import IsParticipantOfConversation
-from .filters import MessageFilter
 from .pagination import MessagePagination
-
+from .filters import MessageFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ConversationViewSet(viewsets.ModelViewSet):
     serializer_class = ConversationSerializer
     permission_classes = [permissions.IsAuthenticated, IsParticipantOfConversation]
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    search_fields = ["participants__first_name", "participants__last_name"]
-    ordering_fields = ["created_at"]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['participants__first_name', 'participants__last_name']
+    ordering_fields = ['created_at']
 
     def get_queryset(self):
         return Conversation.objects.filter(participants=self.request.user).prefetch_related(
-            "participants", "messages__sender"
+            'participants', 'messages__sender'
         )
-
-    def get_object(self):
-        obj = super().get_object()
-        self.check_object_permissions(self.request, obj)
-        return obj
 
     def perform_create(self, serializer):
         conversation = serializer.save()
@@ -38,27 +31,24 @@ class MessageViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     filterset_class = MessageFilter
     pagination_class = MessagePagination
-    ordering_fields = ["sent_at"]
+    ordering_fields = ['sent_at']
 
     def get_queryset(self):
-        conversation_id = self.kwargs.get("conversation_pk")
+        conversation_id = self.kwargs.get('conversation_pk')
         conversation = get_object_or_404(Conversation, pk=conversation_id)
 
         if self.request.user not in conversation.participants.all():
-            return Message.objects.none()
+            from rest_framework import status
+            raise PermissionDenied(detail="You are not a participant of this conversation.")
 
-        return Message.objects.filter(conversation=conversation).select_related("sender")
-
-    def get_object(self):
-        obj = super().get_object()
-        self.check_object_permissions(self.request, obj)
-        return obj
+        return Message.objects.filter(conversation=conversation).select_related('sender')
 
     def perform_create(self, serializer):
-        conversation_id = self.kwargs.get("conversation_pk")
+        conversation_id = self.kwargs.get('conversation_pk')
         conversation = get_object_or_404(Conversation, pk=conversation_id)
 
         if self.request.user not in conversation.participants.all():
-            raise PermissionDenied("You are not a participant of this conversation.")
+            from rest_framework import status
+            raise PermissionDenied(detail="You are not a participant of this conversation.")
 
         serializer.save(sender=self.request.user, conversation=conversation)
